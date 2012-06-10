e927b1083de9a2d2ce80a4c361c772b6df8c98ce
Piotr Rajki <Sedzia1990@gmail.com>
2012-06-10 19:05:12 +0200

    poprawa stringów na Guid + inne poprawki

--- Photolife/Models/Message.cs
+++ Photolife/Models/Message.cs
@@ -10,14 +10,21 @@ namespace Photolife.Models
     {
         public int MessageId { get; set; }
 
-        public string Nadawca { get; set; }
+        public Guid Nadawca { get; set; }
 
 
+        public Guid Odbiorca { get; set; }
+
+        public string Tytuł { get; set; }
+
+        public string Treść { get; set; }
+    }
+    public class AddingMessage
+    {
         public string Odbiorca { get; set; }
 
         public string Tytuł { get; set; }
 
         public string Treść { get; set; }
-
     }
 }
