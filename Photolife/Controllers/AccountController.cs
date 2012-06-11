33221a62eec3a7aaedd4d56197eafdf1605171cd
Sedzia1990 <Sedzia1990@gmail.com>
2012-06-11 22:49:52 +0300

    Update master

--- Photolife/Controllers/AccountController.cs
+++ Photolife/Controllers/AccountController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -237,7 +237,6 @@ namespace Photolife.Controllers
                         }
                     }
                 }
-
             
 
            // return View();
